5787cd51e0e1190fcf01154fb0b042fdbd4e84ac
Andrés Ramírez Quirós <andrew.rq@gmail.com>
2018-10-06 17:13:27 -0600

    Documentación interna y build implementado

--- proyecto2/include/ResistorGrid.hpp
+++ proyecto2/include/ResistorGrid.hpp
@@ -47,6 +47,10 @@ namespace anpi{
     /// ... constructors and other methods
 
     /// Constructor
+    
+    /**
+     * Constructor that initialize a matrix 'A' and a vector 'b' with the size necesary once the rawMap_ is loaded
+     */
     ResistorGrid() {
       size_t ASize = 2*rawMap_.rows()*rawMap_.cols()-(rawMap_.rows()+rawMap_.cols());
       A_.allocate(ASize,ASize);
@@ -55,6 +59,13 @@ namespace anpi{
       b_.resize(ASize);
     }
     
+    /**
+     * Constructor of the class that initialize an empty map of row*cols,
+     * a solution matrix 'A' of size depending of the variables
+     * and a 'b' vector of size equal to 'A'
+     * @param rows number of rows of rawMap_
+     * @param cols number of columns of rawMap_
+     */
     ResistorGrid(size_t rows, size_t cols) {
       rawMap_.allocate(rows,cols);
       rawMap_.fill(0.0f);
@@ -67,17 +78,29 @@ namespace anpi{
       b_.resize(ASize);
     }
 
-    std::size_t extremos(std::size_t row1,
-                          std::size_t col1,
-                          std::size_t row2,
-                          std::size_t col2) {
+    /**
+     * Function that returns the index of the resistance at the edges of the grid
+     * @param row1 row of the node left of the resistance
+     * @param col1 column of the node left of the resistance
+     * @param row2 row of the node right of the resistance
+     * @param col2 column of the node right of the resistance
+     * @return
+     */
+    std::size_t edgeResistance(std::size_t row1,
+                               std::size_t col1,
+                               std::size_t row2,
+                               std::size_t col2) {
       return (col2-1) + (col2+1)*row2 + col2*row1;
     }
 
     /**
-     * Convert a pair of nodes coordinates to an index
+     * Convert a pair of nodes coordinates to an index, unique to each resistance
+     * @param row1 row of the left node
+     * @param col1 column of the left node
+     * @param row2 row of the right column
+     * @param col2 column of the right column
+     * @return index of the resistance
      */
-
     std::size_t nodesToIndex(std::size_t row1,
                              std::size_t col1,
                              std::size_t row2,
@@ -90,9 +113,9 @@ namespace anpi{
           size_t n = rawMap_.cols()-1;
       
           if(col1 == col2) {    //Vertical case
-            idmax = extremos(row1, n, row2, n);
+            idmax = edgeResistance(row1, n, row2, n);
           } else {               //Horizontal case
-            idmax = extremos(row1, (n-1), row2, n);
+            idmax = edgeResistance(row1, (n - 1), row2, n);
           }
       
           idx = idmax - (n-col2);
@@ -106,14 +129,16 @@ namespace anpi{
     }
 
     /**
-     * Convert an index to the pair of nodes coordinates
+     * Convert an index to the pair of nodes coordinates of the resistance with that index
+     * @param idx index of the resistance
+     * @return struct indePair with the pair of nodes before and after the resistance located in the rawMap_
      */
     indexPair indexToNodes(std::size_t idx) {
   
       if (((2*rawMap_.cols()*rawMap_.rows())-
            (rawMap_.cols()+rawMap_.rows())) > idx) {
     
-        indexPair result;
+        indexPair result{};
         size_t n = rawMap_.cols()-1;
         size_t horizontal, vertical;
     
@@ -146,7 +171,29 @@ namespace anpi{
      * Construct the grid from the given file
      * @return true if successful or false otherwise
      */
-    bool build(std::string filename);
+    bool build(const std::string &filename) {
+      // Build the name of the image in the data path
+      std::string mapPath = std::string( ANPI_DATA_PATH ) + filename;
+  
+      // Read the image using the OpenCV
+      cv::Mat_<float> map;
+  
+      cv::imread(mapPath.c_str(),
+                 CV_LOAD_IMAGE_GRAYSCALE).convertTo(map,CV_32FC1);
+      map /= 255.0f; // normalize image range to 0 .. 255
+  
+      // Convert the OpenCV matrix into an anpi matrix
+      // We have to use the std::allocator to avoid an exact stride
+      anpi::Matrix<float,std::allocator<float> > amapTmp(static_cast<const size_t>(map.rows),
+                                                         static_cast<const size_t>(map.cols),
+                                                         map.ptr<float>());
+      // And transform it to a SIMD-enabled matrix
+      anpi::Matrix<float> amap(amapTmp);
+  
+      rawMap_ = amap;
+  
+      return true;
+    }
 
     /**
      * Compute the internal data to navigate between the given nodes
